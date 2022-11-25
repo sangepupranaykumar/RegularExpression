1ff1f81ce3e0a847b70277d70d807f1d62635c8c
Sangepu Pranay Kumar <pranaychinna12@gmail.com>
2022-11-25 14:37:09 +0530

    Checking vowel or consonant

---
+++ src/com/LogicalPrograms/VowelOrConsonant.java
@@ -0,0 +1,27 @@
+package com.LogicalPrograms;
+
+import java.util.Scanner;
+public class VowelOrConsonant {
+    public static void main(String[] args) {
+        System.out.println("Enter any alphabet ");
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(0);
+        switch (c)
+        {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+                System.out.println(c+" is a vowel ");
+                break;
+            default:
+                System.out.println(c+" is a consonant ");
+        }
+    }
+}
