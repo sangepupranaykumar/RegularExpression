1e0d6d62afb7c40f0e6881bbc2931750a450d509
Sangepu Pranay Kumar <pranaychinna12@gmail.com>
2022-11-25 14:33:47 +0530

    Reversing a number using for loop

---
+++ src/com/LogicalPrograms/ReverseNum.java
@@ -0,0 +1,15 @@
+package com.LogicalPrograms;
+
+public class ReverseNum {
+    public static void main(String[] args) {
+        int num = 231, reversed = 0;
+
+        for(;num != 0; num /= 10) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+        }
+
+        System.out.println("Reversed Number: " + reversed);
+
+    }
+}
