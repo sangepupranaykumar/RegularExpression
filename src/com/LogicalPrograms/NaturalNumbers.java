6fa205a2af6fdea0cb15e595d7c7bf62cc15466e
Sangepu Pranay Kumar <pranaychinna12@gmail.com>
2022-11-25 14:29:51 +0530

    Natural numbers sum using while loop

---
+++ src/com/LogicalPrograms/NaturalNumbers.java
@@ -0,0 +1,14 @@
+package com.LogicalPrograms;
+
+public class NaturalNumbers {
+    public static void main(String[] args) {
+        int sum =0;
+        int i = 1;
+        while (i<6){
+            sum = sum+i;
+            i++;
+        }
+        System.out.println("Sum of N natural numbers are "+sum);
+
+    }
+}
