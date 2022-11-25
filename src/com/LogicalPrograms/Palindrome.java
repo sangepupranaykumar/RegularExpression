26186b6c161dacd8f4bff6363a226a433dc467b0
Sangepu Pranay Kumar <pranaychinna12@gmail.com>
2022-11-25 14:30:55 +0530

    Palindrome for a given Number

---
+++ src/com/LogicalPrograms/Palindrome.java
@@ -0,0 +1,22 @@
+package com.LogicalPrograms;
+
+import java.util.Scanner;
+public class Palindrome {
+    public static void main(String[] args) {
+        int num,t,s,rem;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any number ");
+        num=sc.nextInt();
+        t=num;
+        for(s=0;num>0;num/=10)
+        {
+            rem=num%10;
+            s=(s*10)+rem;
+        }
+        if(s==t)
+            System.out.println(t+" is a palindrome number ");
+        else
+            System.out.println(t+" is not a palindrome number ");
+
+    }
+}
