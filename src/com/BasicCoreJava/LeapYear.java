45b1566d02be0de83c134b35357c5443abf0a05d
Sangepu Pranay Kumar <pranaychinna12@gmail.com>
2022-11-27 19:19:06 +0530

    Checking weather leap year or not

---
+++ src/com/BasicCoreJava/LeapYear.java
@@ -0,0 +1,16 @@
+package com.BasicCoreJava;
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter Year : ");
+        int year = scan.nextInt();
+
+        if( year%4==0 && year%100!=0 || year%400==0)
+            System.out.println("The Year is a Leap Year");
+        else
+            System.out.println("The Year is NOT a Leap Year");
+    }
+}
