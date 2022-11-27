713226719c41715e5ed89743d780c15249980e73
Sangepu Pranay Kumar <pranaychinna12@gmail.com>
2022-11-27 23:23:00 +0530

    Finding Second Largest Element

---
+++ src/com/arrayprograms/DuplicateElements.java
@@ -0,0 +1,14 @@
+package com.arrayprograms;
+
+public class DuplicateElements {
+    public static void main(String[] args) {
+        int [] arr = new int[] {1, 2, 3, 4, 5, 2, 3, 5};
+        System.out.println("Duplicate elements in array :");
+       for (int i = 0; i< arr.length; i++){
+           for (int j=i+1;j< arr.length; j++){
+               if (arr[i]==arr[j])
+                   System.out.println(arr[j]);
+           }
+       }
+    }
+}
