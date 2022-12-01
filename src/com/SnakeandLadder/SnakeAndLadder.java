7bca7a306f8a724c7df023cd9a860ed5e4757457
Sangepu Pranay Kumar <pranaychinna12@gmail.com>
2022-12-01 12:28:57 +0530

    Welcome to snake and ladder

---
+++ src/com/SnakeandLadder/SnakeAndLadder.java
@@ -0,0 +1,7 @@
+package com.SnakeandLadder;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Snake and Ladder game");
+    }
+}
