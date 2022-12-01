2a1761cb54ce607c09d24d25534e53dc0bfaffb8
Sangepu Pranay Kumar <pranaychinna12@gmail.com>
2022-12-01 12:31:07 +0530

    Initial position of the player

---
+++ src/com/SnakeandLadder/PlayerPosition.java
@@ -0,0 +1,10 @@
+package com.SnakeandLadder;
+
+public class PlayerPosition {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Snake and Ladder ");
+        int userPosition =0;
+        System.out.println();
+        System.out.println("Initial Position of the Player"+userPosition);
+    }
+}
