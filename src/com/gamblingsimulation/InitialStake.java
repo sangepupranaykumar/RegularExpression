cef8aa16b8bafb7a94ead4f2558d05f3c3edc40e
Sangepu Pranay Kumar <pranaychinna12@gmail.com>
2022-12-07 02:22:19 +0530

    Starting with Initial Stake and Betting every game

---
+++ src/com/gamblingsimulation/InitialStake.java
@@ -0,0 +1,16 @@
+package com.gamblingsimulation;
+
+public class InitialStake {
+    /*
+     * Taking initial Stake & bet variables and its assigned values
+     */
+    public static final  int initialStake = 100;
+    public static final int stakeBet = 1;
+
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Gambling Game Simulator program");
+        System.out.println("Initial Stake is " +initialStake +"$");
+        System.out.println("Stake Bet is " +stakeBet+"$");
+    }
+}
