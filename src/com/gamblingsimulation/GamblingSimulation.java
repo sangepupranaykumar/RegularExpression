09ca6afc403ed2201a084ce5027d176ea346dcaf
Sangepu Pranay Kumar <pranaychinna12@gmail.com>
2022-12-07 02:19:54 +0530

    Welcome to Gambling Simulator Problem

---
+++ src/com/gamblingsimulation/GamblingSimulation.java
@@ -0,0 +1,7 @@
+package com.gamblingsimulation;
+
+public class GamblingSimulation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Gambling Simulation Problem ");
+    }
+}
