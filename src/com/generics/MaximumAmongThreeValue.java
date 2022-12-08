7dad5d791ef1200be47e9acb8eb52f4269668c10
Sangepu Pranay Kumar <pranaychinna12@gmail.com>
2022-12-08 12:52:03 +0530

    Refactoring all the three to one generic Method

---
+++ src/com/generics/MaximumAmongThreeValue.java
@@ -0,0 +1,21 @@
+package com.generics;
+
+public class MaximumAmongThreeValue<T>{
+    T num1, num2, num3;
+    public static <T extends Comparable<T>> T maxAmong3Number(T num1, T num2, T num3)
+    {
+        T max = num1;
+        if(num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
+            max = num1;
+        else if(num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0)
+            max = num2;
+        else
+            max = num3;
+        return max;
+    }
+    public static void main(String[] args) {
+        System.out.println("The max Number Among 3 String numbers is: "+maxAmong3Number("Apple", "Dragonfruit", "Banana"));
+        System.out.println("The max Number Among 3 Integer numbers is: "+maxAmong3Number(1504,22,27));
+        System.out.println("The max Number Among 3 Float numbers is: "+maxAmong3Number(32.5f,56.98f,78.99f));
+    }
+}
