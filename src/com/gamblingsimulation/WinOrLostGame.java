8160a6d7928becdc019bfffe7251d29429725883
Sangepu Pranay Kumar <pranaychinna12@gmail.com>
2022-12-12 14:33:49 +0530

    Checking the stake to resign game

---
+++ src/com/gamblingsimulation/WinOrLostGame.java
@@ -0,0 +1,27 @@
+package com.gamblingsimulation;
+import java.util.Random;
+public class WinOrLostGame {
+    public static final int initialStake = 100;
+    public static final int stakeBet = 51;
+    static Random random = new Random();
+
+    /*
+     * Getting the chance of Winning or Loosing game by betting 1$ Using Switch-Case
+     */
+    public static void main(String[] args) {
+        int totalStake = initialStake;
+        System.out.println("Welcome to Gambling Game Simulator program");
+        int play = random.nextInt(2);
+        switch (play) {
+            case 0:
+                totalStake = totalStake - stakeBet;
+                System.out.println("Stake after losing: " + totalStake);
+                System.out.println("Gambler resigns the game");
+                break;
+            case 1:
+                totalStake = totalStake + stakeBet;
+                System.out.println("Stake after winning: " + totalStake);
+                break;
+        }
+    }
+}
