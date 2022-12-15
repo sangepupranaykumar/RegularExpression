ced7b19e64d7a0db05c1259ed7da0f4143a045f2
Sangepu Pranay Kumar <pranaychinna12@gmail.com>
2022-12-15 14:33:47 +0530

    Deleting the last Node

---
+++ src/com/linkedlist/DeleteLastNode.java
@@ -0,0 +1,94 @@
+package com.linkedlist;
+
+public class DeleteLastNode {
+    Node head;
+
+    class Node {
+        Object data;
+        Node ref;
+
+        Node(Object data) {
+            this.data = data;
+        }
+    }
+
+    public void addNodeFirst(Object data) {
+        Node newNode = new Node(data);
+        newNode.ref = head;
+        head = newNode;
+    }
+
+    public void addNodeLast(Object data) {
+        Node newNode = new Node(data);
+        Node temp = head;
+        if (head == null)
+            head = newNode;
+        else if (head.ref == null)
+            head.ref = newNode;
+        else {
+            while (temp.ref != null) {
+                temp = temp.ref;
+            }
+            temp.ref = newNode;
+        }
+
+    }
+
+    public void addNodeBetween(Object data, int position) {
+        Node newNode = new Node(data);
+        int index = 0;
+        Node left = head;
+        Node right = left.ref;
+        while (index < (position - 1)) {
+            left = left.ref;
+            right = right.ref;
+            index++;
+        }
+        newNode.ref = right;
+        left.ref = newNode;
+    }
+
+    public void deleteNodeFirst() {
+        head = head.ref;
+    }
+
+    public void deleteNodeLast() {
+        if (head == null)
+            System.out.println("Linked List is Empty");
+        else if (head.ref == null)
+            head = null;
+        else {
+            Node temp = head;
+            while (temp.ref.ref != null) {
+                temp = temp.ref;
+            }
+            temp.ref = null;
+        }
+    }
+
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            if (temp.ref != null)
+                System.out.print(temp.data + "->");
+            else
+                System.out.println(temp.data);
+            temp = temp.ref;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        DeleteLastNode obj = new DeleteLastNode();
+        obj.addNodeFirst(56);
+        obj.display();
+        obj.addNodeLast(70);
+        obj.display();
+        obj.addNodeBetween(30, 1);
+        obj.display();
+        obj.deleteNodeFirst();
+        obj.display();
+        obj.deleteNodeLast();
+        obj.display();
+    }
+}
