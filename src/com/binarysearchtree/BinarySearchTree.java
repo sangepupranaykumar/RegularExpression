34aec5e6b7ced7eaf0aedcc2c1a41ff79ef121ad
Sangepu Pranay Kumar <pranaychinna12@gmail.com>
2022-12-15 18:31:44 +0530

    Welcome to Binary Search Tree

---
+++ src/com/binarysearchtree/BinarySearchTree.java
@@ -0,0 +1,7 @@
+package com.binarysearchtree;
+
+public class BinarySearchTree {
+    public static void main(String[] args) {
+        System.out.println("Welcome to binary search tree");
+    }
+}
